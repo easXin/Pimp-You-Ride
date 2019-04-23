5391cb1ef2c1cd3324365cfcff539b751e22d786
es1294 <johnbola@buffalo.edu>
2019-04-23 15:48:54 -0400

    added background dropdown for ide

--- src/components/menus/Backgrounds.vue
+++ src/components/menus/Backgrounds.vue
@@ -1,14 +1,32 @@
 <template>
 <div>
     <h1> Backgrounds </h1>
+    <v-list>
+        <v-list-tile
+        v-for = "temp in backgrounds"
+        :key ="temp"
+        >
+        <img :src = "temp.path" width="200" height="50">
+        <v-icon :id="temp+'icon'" class='d-none' color="green">checkmark</v-icon>
+
+        </v-list-tile>
+    </v-list>
 </div>
     
 </template>
 <script>
+
+import {Backgrounds} from '../../settings.js'
 export default {
-    name: "Backgrounds"
+    name: "Backgrounds",
+    data(){
+        return{
+            backgrounds: Backgrounds
+        }
+    }
     
 }
+
 </script>
 
 <style scoped>
--- src/settings.js
+++ src/settings.js
@@ -9,3 +9,19 @@ export const FONT_SETTINGS = {
     [ROBOTO]: "'Roboto Condensed', sans-serif",
     [PATUA_ONE]: "'Patua One', cursive"
 }
+
+export const Backgrounds = [{
+    title: 'Space',
+    dark: true,
+    path: 'http://i65.tinypic.com/hwmwdw.jpg'
+},
+{
+    title: 'Clouds',
+    dark: false,
+    path: 'http://i64.tinypic.com/10rq2df.jpg'
+},
+{
+    title: 'Deathstar',
+    dark: true,
+    path: 'http://i65.tinypic.com/2m5jyc6.jpg'
+}]

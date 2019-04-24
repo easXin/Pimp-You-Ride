856d1ac0ce59b3bc804707bd394b76e1990f55b6
Daniel <dmconnol@buffalo.edu>
2019-04-24 13:10:40 -0400

    Cleaning up master

--- src/views/IDE.vue
+++ src/views/IDE.vue
@@ -1,9 +1,5 @@
 <template>
-<<<<<<< HEAD
-    <div id="entireIDEPage">
-=======
     <div id="page">
->>>>>>> backgrounds
        
         
         <b-container>
@@ -54,6 +50,10 @@ export default {
             page.style.background = "url("+this.backgrounds[background].path + ")";
             let ide = document.getElementById("ide")
             ide.style.background =  "white"
+            if(this.backgrounds[background].dark){
+                page.style.color = "white";
+                ide.style.color = "black";
+            }
 
         },
         submit(){
